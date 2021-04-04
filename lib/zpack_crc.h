5426368b61533e7fa1e8fe245eb335df3fca3c14
LeadRDRK <25358041+LeadRDRK@users.noreply.github.com>
2021-04-04 21:23:26 +0700

    major restructuring
    * Updated the specs
    * Changed the function naming convention
    * More robust error handling system
    * Fixed the CLI's behavior on directories

--- lib/zpack_crc.h
+++ lib/zpack_crc.h
@@ -6,9 +6,8 @@ class CRC32
 {
     public:
         CRC32();
-        void Add(char* buf, size_t size);
-        uint32_t Get();
-        operator uint32_t() { return Get(); };
+        void add(char* buf, size_t size);
+        uint32_t digest();
 
     private:
         uint32_t crc;
