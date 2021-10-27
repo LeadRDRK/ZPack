b554832ae6593e654f8a7c73cb4f1cb74b7c4f5a
LeadRDRK <leadrdrk@zohomail.com>
2021-10-27 20:46:39 +0700

    Updated API names, groundwork for streaming API
    - zpack_archive -> zpack_reader
    - zpack_stream added

--- tests/open_archive.c
+++ tests/open_archive.c
@@ -2,13 +2,13 @@
 #include <string.h>
 #include "archive.h"
 
-zpack_bool print_and_verify_archive(zpack_archive* archive)
+zpack_bool print_and_verify_archive(zpack_reader* reader)
 {
-    zpack_file_entry* entries = archive->file_entries;
-    printf("File count: %lu\n\n", archive->file_count);
+    zpack_file_entry* entries = reader->file_entries;
+    printf("File count: %lu\n\n", reader->file_count);
 
     zpack_bool passed = ZPACK_TRUE;
-    for (zpack_u64 i = 0; i < archive->file_count; ++i)
+    for (zpack_u64 i = 0; i < reader->file_count; ++i)
     {
         zpack_bool entry_passed = (
             strcmp(entries[i].filename, _filenames[i]) == 0 &&
@@ -41,48 +41,48 @@ int open_archive(int num)
     printf("Archive #%d\n"
            "----------------------\n", num);
 
-    zpack_archive archive;
-    memset(&archive, 0, sizeof(archive));
+    zpack_reader reader;
+    memset(&reader, 0, sizeof(reader));
     int ret;
 
     // read from disk
     printf("File read test\n");
 
-    if ((ret = zpack_open_archive(&archive, _archive_names[num])))
+    if ((ret = zpack_init_reader(&reader, _archive_names[num])))
     {
         printf("Error %d\n", ret);
         return 1;
     }
 
-    zpack_bool passed1 = print_and_verify_archive(&archive);
-    zpack_close_archive(&archive);
+    zpack_bool passed1 = print_and_verify_archive(&reader);
+    zpack_close_reader(&reader);
 
     // read from buffer
     printf("Buffer read test\n");
 
     zpack_u64 size = _archive_sizes[num];
-    if ((ret = zpack_open_archive_memory(&archive, _archive_buffers[num], size)))
+    if ((ret = zpack_init_reader_memory(&reader, _archive_buffers[num], size)))
     {
         printf("Error %d\n", ret);
         return 1;
     }
 
-    zpack_bool passed2 = print_and_verify_archive(&archive);
-    zpack_close_archive(&archive);
+    zpack_bool passed2 = print_and_verify_archive(&reader);
+    zpack_close_reader(&reader);
 
     // read from buffer (shared)
     printf("Buffer read test (shared)\n");
 
     zpack_u8 tmp[size];
     memcpy(tmp, _archive_buffers[num], size);
-    if ((ret = zpack_open_archive_memory_shared(&archive, tmp, size)))
+    if ((ret = zpack_init_reader_memory_shared(&reader, tmp, size)))
     {
         printf("Error %d\n", ret);
         return 1;
     }
 
-    zpack_bool passed3 = print_and_verify_archive(&archive);
-    zpack_close_archive(&archive);
+    zpack_bool passed3 = print_and_verify_archive(&reader);
+    zpack_close_reader(&reader);
 
     // 0 if passed, 1 if failed
     return !(passed1 && passed2 && passed3);
--- tests/read_archive.c
+++ tests/read_archive.c
@@ -3,25 +3,25 @@
 #include "archive.h"
 
 #define BUFFER_SIZE 350
-zpack_bool read_and_verify_files(zpack_archive* archive, zpack_u8* buffer)
+zpack_bool read_and_verify_files(zpack_reader* reader, zpack_u8* buffer)
 {
     zpack_bool passed = ZPACK_TRUE;
     int ret;
 
     // Oneshot decompression
     printf("* Oneshot\n");
-    for (int i = 0; i < archive->file_count; ++i)
+    for (int i = 0; i < reader->file_count; ++i)
     {
-        if ((ret = zpack_archive_read_file(archive, &archive->file_entries[i], buffer, BUFFER_SIZE)))
+        if ((ret = zpack_read_file(reader, &reader->file_entries[i], buffer, BUFFER_SIZE)))
         {
-            printf("Failed to read %s (error %d, last return %ld)\n", archive->file_entries[i].filename, ret, archive->last_return);
+            printf("Failed to read %s (error %d, last return %ld)\n", reader->file_entries[i].filename, ret, reader->last_return);
             passed = ZPACK_FALSE;
             continue;
         }
 
-        zpack_bool valid = memcmp(buffer, _files[i], archive->file_entries[i].uncomp_size) == 0;
+        zpack_bool valid = memcmp(buffer, _files[i], reader->file_entries[i].uncomp_size) == 0;
         passed = passed ? valid : ZPACK_FALSE;
-        printf("-- %s is %s\n", archive->file_entries[i].filename, valid ? "valid" : "invalid");
+        printf("-- %s is %s\n", reader->file_entries[i].filename, valid ? "valid" : "invalid");
     }
 
     // Streaming decompression
@@ -35,8 +35,8 @@ int read_archive(int num)
     printf("Archive #%d\n"
            "----------------------\n", num);
     
-    zpack_archive archive;
-    memset(&archive, 0, sizeof(archive));
+    zpack_reader reader;
+    memset(&reader, 0, sizeof(reader));
 
     int ret;
     zpack_u8 buffer[BUFFER_SIZE];
@@ -44,41 +44,41 @@ int read_archive(int num)
     // read from disk
     printf("File read test\n");
 
-    if ((ret = zpack_open_archive(&archive, _archive_names[num])))
+    if ((ret = zpack_init_reader(&reader, _archive_names[num])))
     {
         printf("Failed to open archive (error %d)\n", ret);
         return 1;
     }
 
-    zpack_bool passed1 = read_and_verify_files(&archive, buffer);
-    zpack_close_archive(&archive);
+    zpack_bool passed1 = read_and_verify_files(&reader, buffer);
+    zpack_close_reader(&reader);
 
     // read from buffer
     printf("Buffer read test\n");
 
     zpack_u64 size = _archive_sizes[num];
-    if ((ret = zpack_open_archive_memory(&archive, _archive_buffers[num], size)))
+    if ((ret = zpack_init_reader_memory(&reader, _archive_buffers[num], size)))
     {
         printf("Failed to open archive (error %d)\n", ret);
         return 1;
     }
 
-    zpack_bool passed2 = read_and_verify_files(&archive, buffer);
-    zpack_close_archive(&archive);
+    zpack_bool passed2 = read_and_verify_files(&reader, buffer);
+    zpack_close_reader(&reader);
 
     // read from buffer (shared)
     printf("Buffer read test (shared)\n");
 
     zpack_u8 tmp[size];
     memcpy(tmp, _archive_buffers[num], size);
-    if ((ret = zpack_open_archive_memory_shared(&archive, tmp, size)))
+    if ((ret = zpack_init_reader_memory_shared(&reader, tmp, size)))
     {
         printf("Failed to open archive (error %d)\n", ret);
         return 1;
     }
 
-    zpack_bool passed3 = read_and_verify_files(&archive, buffer);
-    zpack_close_archive(&archive);
+    zpack_bool passed3 = read_and_verify_files(&reader, buffer);
+    zpack_close_reader(&reader);
 
     // 0 if passed, 1 if failed
     return !(passed1 && passed2 && passed3);
